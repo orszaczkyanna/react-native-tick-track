2fb12692d7352824da95e69810ad84306eca94ea
orszaczkyanna <orszaczky.anna@gmail.com>
2024-10-11 17:50:55 +0200

    Add real-time Clock with ticking sound

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,25 +1,54 @@
-import { View, Text, TouchableOpacity } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { useState, useEffect, useRef } from "react";
+import { useSoundContext } from "@/context/SoundContext";
+import Container from "@/components/Container";
+import TimeDisplay from "@/components/TimeDisplay";
+import MuteButton from "@/components/MuteButton";
 
 const Clock = () => {
+  const { playSound, isMuted } = useSoundContext();
+
+  const [currentTime, setCurrentTime] = useState<number>(Date.now());
+  const timeoutIdRef = useRef<NodeJS.Timeout | null>(null); // Initialize with null to prevent errors during cleanup checks
+
+  // Update the currentTime state
+  useEffect(() => {
+    const tick = () => {
+      setCurrentTime(Date.now());
+
+      if (!isMuted) {
+        playSound();
+      }
+
+      timeoutIdRef.current = setTimeout(tick, 1000);
+    };
+
+    tick();
+
+    return () => {
+      if (timeoutIdRef.current) clearTimeout(timeoutIdRef.current);
+    };
+  }, [isMuted]);
+
+  // Format time to a two-digit string
+  const formatTime = (time: number): string => {
+    return String(time).padStart(2, "0");
+  };
+
+  // Display the current time in hours, minutes, and seconds
+  const displayTime = (): string => {
+    const date = new Date(currentTime);
+    const hours = formatTime(date.getHours());
+    const minutes = formatTime(date.getMinutes());
+    const seconds = formatTime(date.getSeconds());
+
+    return `${hours}:${minutes}:${seconds}`;
+  };
+
   return (
-    <SafeAreaView className="bg-background flex-1 justify-center items-center">
-      <Text className="text-foreground text-6xl font-monregular">16:08:39</Text>
-      <View className="flex-row mt-5 space-x-5">
-        <TouchableOpacity
-          activeOpacity={0.7}
-          className="bg-background-100 min-h-[64px] min-w-[160px] justify-center items-center rounded-full"
-        >
-          <Text className="text-foreground text-xl font-monmedium">Cancel</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          activeOpacity={0.7}
-          className="bg-accent-100 min-h-[64px] min-w-[160px] justify-center items-center rounded-full"
-        >
-          <Text className="text-foreground text-xl font-monmedium">Start</Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
+    <Container>
+      <MuteButton />
+      <TimeDisplay>{displayTime()}</TimeDisplay>
+    </Container>
   );
 };
 
